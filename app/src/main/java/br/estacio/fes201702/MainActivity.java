debc5ee2d6f8ec3b713581b44a0d1adc89afe1a1
carlos <carlos@quiantati.com.br>
2017-09-12 19:24:11 -0400

    Versão Inicial para o dia 12/09/2017.

--- app/src/main/java/br/estacio/fes201702/MainActivity.java
+++ app/src/main/java/br/estacio/fes201702/MainActivity.java
@@ -2,12 +2,37 @@ package br.estacio.fes201702;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtNome, edtFone, edtEmail;
+    private Button btnSalvar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNome = (EditText) findViewById(R.id.edtNome);
+        edtFone = (EditText) findViewById(R.id.edtFone);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        btnSalvar = (Button) findViewById(R.id.btnSalvar);
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        String.format("O nome digitado foi %s e o email foi %s.",
+                                edtNome.getText(), edtEmail.getText()),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
